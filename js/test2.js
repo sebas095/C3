9812ce97ed46f10412257735ce667c1547e6d248
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-07-07 22:52:12 -0500

    Se añaden eventos show y hide al test2

--- js/test2.js
+++ js/test2.js
@@ -1,3 +1,8 @@
+var btn1 = document.getElementById('sm1');
+var btn2 = document.getElementById('sm2');
+var btn3 = document.getElementById('sm3');
+var btn4 = document.getElementById('sm4');
+
 var data1 = ['sample', random(0, 100)];
 var data2 = ['sample2', random(0, 100)];
 
@@ -9,6 +14,10 @@ var chart = c3.generate({
 });
 
 load(20);
+btn1.addEventListener('click', () => chart.hide(['sample']));
+btn2.addEventListener('click', () => chart.show(['sample']));
+btn3.addEventListener('click', () => chart.hide(['sample2']));
+btn4.addEventListener('click', () => chart.show(['sample2']));
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -26,7 +35,7 @@ function load(points) {
     chart.load({
       columns: [data1, data2]
     });
-    
+
     points--;
     load(points);
   }, 1000);
